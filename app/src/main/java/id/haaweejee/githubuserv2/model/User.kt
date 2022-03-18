6bb396bc923641f076fbd6f220c4e6f77e800aff
magangkompas <52823787+haaweejee@users.noreply.github.com>
2022-03-18 20:42:20 +0700

    + refactor and add 2 models to retrofit

--- app/src/main/java/id/haaweejee/githubuserv2/model/User.kt
+++ app/src/main/java/id/haaweejee/githubuserv2/model/User.kt
@@ -3,10 +3,8 @@ package id.haaweejee.githubuserv2.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class User(
-    //TODO: If You want add some attribute or data just add the variabel name
-    val username: String,
-    val name: String,
-    val avatar : Int,
-): Parcelable
+    val id: Int,
+    val login: String,
+    val avatar_url : String
+)
