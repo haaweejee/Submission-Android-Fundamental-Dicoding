63566f2dd067d65d132e9d2853fc954ce338a7bb
magangkompas <52823787+haaweejee@users.noreply.github.com>
2022-03-18 20:44:31 +0700

    ! update adapter

--- app/src/main/java/id/haaweejee/githubuserv2/adapter/UserListAdapter.kt
+++ app/src/main/java/id/haaweejee/githubuserv2/adapter/UserListAdapter.kt
@@ -1,9 +1,12 @@
 package id.haaweejee.githubuserv2.adapter
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import id.haaweejee.githubuserv2.DetailActivity
 import id.haaweejee.githubuserv2.databinding.ItemCardLayoutBinding
 import id.haaweejee.githubuserv2.model.User
@@ -12,15 +15,28 @@ class UserListAdapter : RecyclerView.Adapter<UserListAdapter.ListViewHolder>() {
 
     private val listUser = ArrayList<User>()
 
+    private var onItemClickCallback: OnItemClickCallback? = null
+
+    fun setOnItemClick (onItemClickCallback: OnItemClickCallback){
+        this.onItemClickCallback = onItemClickCallback
+    }
+
     fun setData(user: List<User>){
         listUser.clear()
         listUser.addAll(user)
+        notifyDataSetChanged()
+        Log.d("TAG", "cek $listUser")
     }
 
     inner class ListViewHolder(private val binding: ItemCardLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
         internal fun bind(user: User){
-            binding.tvUsername.text = user.username
-            binding.ivAvatar.setImageResource(user.avatar)
+            binding.root.setOnClickListener { onItemClickCallback?.onItemClicked(user) }
+            binding.tvUsername.text = user.login
+            Glide.with(itemView.context)
+                .load(user.avatar_url)
+                .centerCrop()
+                .apply(RequestOptions().override(60,60))
+                .into(binding.ivAvatar)
         }
     }
 
@@ -35,16 +51,12 @@ class UserListAdapter : RecyclerView.Adapter<UserListAdapter.ListViewHolder>() {
     override fun onBindViewHolder(holder: UserListAdapter.ListViewHolder, position: Int) {
         val user = listUser[position]
         holder.bind(user)
-        holder.itemView.setOnClickListener {
-            val moveDetail = Intent(holder.itemView.context, DetailActivity::class.java)
-            moveDetail.putExtra(DetailActivity.EXTRA_USER_DATA, user)
-            holder.itemView.context.startActivity(moveDetail)
-        }
     }
 
     override fun getItemCount(): Int = listUser.size
+
+    interface OnItemClickCallback{
+        fun onItemClicked(data: User)
+    }
 }
 
-interface onCardClickListener{
-    //TODO: This interface gonna use later when we need more action at itemList
-}
